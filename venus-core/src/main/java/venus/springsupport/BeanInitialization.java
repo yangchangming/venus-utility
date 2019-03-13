6934dd52c21f6424f3872a3ab6a51a60ab704f77
yangchangming <yangchangming@datavens.com>
2019-03-13 12:43:05 +0800

    add:nativejdbcextractor dependence

--- venus-core/src/main/java/venus/springsupport/BeanInitialization.java
+++ venus-core/src/main/java/venus/springsupport/BeanInitialization.java
@@ -201,19 +201,20 @@ public class BeanInitialization implements Initialization {
             _context.registerBeanDefinition("defaultLobHandler", lobHandlerBeanDefinition);
         }
 
-        BeanDefinition oracleLobHandlerBeanDefinition = BeanDefinitionBuilder.genericBeanDefinition(org.springframework.jdbc.support.lob.OracleLobHandler.class).getBeanDefinition();
-        oracleLobHandlerBeanDefinition.setLazyInit(true);
-        if (!_context.containsBeanDefinition("oracleLobHandler")){
-            _context.registerBeanDefinition("oracleLobHandler", oracleLobHandlerBeanDefinition);
-        }
-
         BeanDefinition nativeJdbcExtractorBeanDefinition = BeanDefinitionBuilder
                 .genericBeanDefinition(org.springframework.jdbc.support.nativejdbc.SimpleNativeJdbcExtractor.class).getBeanDefinition();
         nativeJdbcExtractorBeanDefinition.setLazyInit(true);
         if (!_context.containsBeanDefinition("nativeJdbcExtractor")){
             _context.registerBeanDefinition("nativeJdbcExtractor", nativeJdbcExtractorBeanDefinition);
         }
+
+        BeanDefinition oracleLobHandlerBeanDefinition = BeanDefinitionBuilder.genericBeanDefinition(org.springframework.jdbc.support.lob.OracleLobHandler.class).getBeanDefinition();
+        oracleLobHandlerBeanDefinition.setLazyInit(true);
+        if (!_context.containsBeanDefinition("oracleLobHandler")){
+            _context.registerBeanDefinition("oracleLobHandler", oracleLobHandlerBeanDefinition);
+        }
+        oracleLobHandlerBeanDefinition.setDependsOn("nativeJdbcExtractor");
+
         return _context;
     }
-
 }
