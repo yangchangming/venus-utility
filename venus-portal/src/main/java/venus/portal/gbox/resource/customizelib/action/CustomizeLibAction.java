095e75823733f3de95dfd567896d6f2c6241251b
yangchangming <yangchangming@datavens.com>
2019-03-16 21:02:28 +0800

    delete:project venus-portal and venus-web-portal

--- venus-portal/src/main/java/venus/portal/gbox/resource/customizelib/action/CustomizeLibAction.java
+++
@@ -1,8 +0,0 @@
-package venus.portal.gbox.resource.customizelib.action;
-
-import venus.portal.gbox.resource.audiolib.util.IAudioLibConstants;
-import venus.frames.base.action.DefaultDispatchAction;
-
-public class CustomizeLibAction extends DefaultDispatchAction implements IAudioLibConstants {
-
-}
